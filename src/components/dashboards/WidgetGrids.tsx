336f7edd8a1751346367b4307944af998eaefb39
Carlos Daniel Zambrano <cdanzamcol@gmail.com>
2025-03-03 10:04:02 -0600

    Restful getCounter

--- src/components/dashboards/WidgetGrids.tsx
+++ src/components/dashboards/WidgetGrids.tsx
@@ -1,10 +1,22 @@
+'use client';
+
+import { IoCarOutline } from "react-icons/io5";
 import { SimpleWidget } from "./SimpleWidget";
+import { useAppSelector } from "@/store";
 
 export const WidgetGrids = () => {
+
+  const isCart = useAppSelector( state => state.counter.count );
   return (
     <div className="flex flex-wrap p-2 justify-center items-center">
-      <SimpleWidget />
-      <SimpleWidget />
+      <SimpleWidget 
+       title={` ${isCart} `} 
+       subTitle="Productos agregados" 
+       label="Contador" 
+       icon={<IoCarOutline size={70} className="text-blue-600" />}
+       href="/dashboard/counter"
+       />
+      {/* <SimpleWidget /> */}
 
     </div>
   );
